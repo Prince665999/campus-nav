74168f55f75042e2fe026473006b3660d663780a
Prince665999 <principiuskaiza@gmail.com>
2026-09-26 19:22:10 +0300

    the config update

--- mobile/constants/config.js
+++ mobile/constants/config.js
@@ -1,7 +1,7 @@
 // App-wide configuration read from environment variables and shared
 // constants.
 
-const DEFAULT_API_BASE_URL = 'http://192.168.100.148:8000';
+const DEFAULT_API_BASE_URL = 'https://llc-stored-beans-evaluations.trycloudflare.com';
 
 export const API_BASE_URL =
   process.env.EXPO_PUBLIC_API_BASE_URL || DEFAULT_API_BASE_URL;
